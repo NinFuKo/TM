96188011e001af0fc958a1eb77dc8f0919105c15
NinFuKo <ninfuko@gmail.com>
2023-03-21 20:08:35 +0100

    Ajout du système de thread et du choix de loctuteur

--- client.py
+++ client.py
@@ -23,32 +23,64 @@ def recv_text(conn):
 def choose_username(conn):
     username = input("Choose a username : ")
     send_text(conn,username)
+
+def choose_persons_ready(conn):
+    import time
+    while True:
+        print("---")
+        persons_ready = recv_text(conn) 
+
+        k = persons_ready.find(":")
+        persons_ready = persons_ready[k+1:]
+        list_persons = persons_ready.split(" ")
+
+
+        print("Choose with who you want to talk") # manque d'un système de page
+        if list_persons[0] == "":
+            send_text(conn,"005")
+        else:
+            for user in list_persons[:len(list_persons)-1]:   
+                print("-",user)
+            
+            wanted = input("")
+            for user in list_persons:
+                if user.lower() == wanted.lower():
+                    send_text(conn,user)
+        
+        
+
+    
+    
         
 def main():
-    try:
-        conn = initialisation_et_connexion()
-        send_text(conn,"001") # code 001 = ping server
-        code = recv_text(conn) # code 002 = server is up / username is requested
-        while True:
+    while True:
+        try:
+            conn = initialisation_et_connexion()
+            send_text(conn,"001") # code 001 = ping server
+            code = recv_text(conn) # code 002 = server is up / username is requested
             if code == "002":
-                choose_username(conn)
-                break
+                while True:
+                    choose_username(conn)
 
-        code = recv_text(conn)
-        if code == "003":
-            print("Invalid username")
-        if code == "004":
-            print("Valid Username")
-                
-        
-        print("yasss")
-                
-
+                    code = recv_text(conn)
+                    if code == "003":
+                        print("Invalid username")
+                    if code == "004":
+                        print("Valid Username")
+                        break
             
-    except ConnectionRefusedError:
-        print("Error")
+            
+            choose_persons_ready(conn)
+                    
 
-    finally:
-        conn.close()
+                
+        except ConnectionRefusedError:
+            print("Error")
+
+        finally:
+            conn.close()
+            quit = input("Quit ? y/n\n")
+            if quit.lower() != "n":
+                break
 
 main()
--- server.py
+++ server.py
@@ -1,32 +1,47 @@
 import socket
+import threading
+import time
 
 def send_text(conn,text): # permet d'envoyer des données
     text_encoded = text.encode("utf-8")
     conn.sendall(text_encoded)
     print("Server send : ",text)
 
-def recv_text(conn): # permet d'attendre de recevoir des données
+def recv_text(conn,id): # permet d'attendre de recevoir des données
     while True:
         text = conn.recv(1024) # taille du buffer encore à voir
         text = text.decode("utf-8")
-        print("Client send : ",text)
-        return text
-    
-def initialisation(port_num): 
-    global socket
-    host, port = ("", port_num)
-    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket.bind((host,port))
-    print("Server is up !")
+        print("Client",id,"send : ",text)
+        if text != "" or text != " ":
+            return text
 
-def wait_connection():
-    while True:
-        socket.listen()
-        conn, address = socket.accept()
-        print("Client connected")
-        return(conn)
+
+def reset_list(): # Cette fonction remet à zero la liste des utilisateurs
+    with open("username_ip.txt","w") as users_list:
+        users_list.write("")
+
+    print("The list has been resetted")
+
+def return_from_list(i):
+    return_items_list = []
+    with open("username_ip.txt","r") as users_list:
+        for user in users_list:
+            user = user.split(" ") [i]
+            return_items_list.append(user)
+    return(return_items_list)
+
+
+def check_username(user): # A vérifer quand il y aura plusieurs clients
+    for username in return_from_list(0):
+        if username.lower() == user.lower():
+            print("Invalid Username")
+            return("003") # code = 003 : code d'erreur
     
-def ip_and_port(conn):
+    return("004") # code = 004 : nom d'utilisateur valide
+
+
+
+def ip_and_port(conn,id): # retourne l'ip et le port
     conn_str = str(conn)
     l = conn_str.find("raddr")
     conn_str = conn_str[l:]
@@ -36,58 +51,89 @@ def ip_and_port(conn):
     l = conn_str.find(",")
     k = conn_str.rfind(")")
     port = conn_str[l+1:k].strip()
+    print("Client",id,":",ip,":",port)
     return((ip,port))
 
 def add_to_list(username,ip_and_port):
     with open("username_ip.txt","a") as users_list:
-        users_list.write(username + " " + ip_and_port[0] + " " + ip_and_port[1])
-    print(username + " / " + ip_and_port[0] + " / " + ip_and_port[1] + " has been added to the list.")
+        users_list.write(username + " " + ip_and_port[0] + " " + ip_and_port[1] + "\n")
+    print("\n",username + " / " + ip_and_port[0] + " / " + ip_and_port[1] + " has been added to the list.\n")
 
-def reset_list():
-    with open("username_ip.txt","w") as users_list:
-        users_list.write("")
-    print("The list has been resetted")
 
-def return_from_list(i):
-    return_items_list = []
-    with open("username_ip.txt","r") as users_list:
-        for user in users_list:
-            user = user.split(" ") [i]
-            return_items_list.append(user)
-            
-    return(return_items_list)
 
-def check_username(user): # A vérifer quand il y aura plusieurs clients
-    for username in return_from_list(0):
-        if username == user:
-            return("003")
+
+def initialisation(port_num): # Initialise le serveur sur un port
+    global socket
+    host, port = ("", port_num)
+    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.bind((host,port))
+    print("Server is up on port",port_num,"!")
+
+
+
+def persons_ready(username): # Retourne la liste des gens prêts
+    in_list_raw = return_from_list(0)
+    in_list = "list :"
+    for user in in_list_raw:
+        if user.lower() != username.lower(): # Permet d'enlever le nom d'utilisateur de la personne à qui on envoie la liste
+            in_list += user
+            in_list += " "
+    return(in_list)
+
+
+
+def wait_connection(id): # Attends une connection
+    while True:
+        socket.listen()
+        conn, addr = socket.accept()
+        print("Client",id,"connected !")
+        thread_with_client = threading.Thread(target=connection_with_client,args=(conn,id))
+
+        thread_with_client.start()
+        return()
+
+
+
+
+def connection_with_client(conn,id): # communication avec client
+    print("Thread with client",id,"started")
+    code = recv_text(conn,id)
+    if code == "001": send_text(conn, "002")
     
-    return("004")
-            
+    (ip,port) = ip_and_port(conn,id)
+
+    while True:
+        username = recv_text(conn,id)
+        code = check_username(username)
+        send_text(conn,code)
+        if code == "004": break
+
     
-
-def main(): # fonction principale du programme
-    reset_list()
-    initialisation(5566)
-    conn = wait_connection()
-
-    code = recv_text(conn)
-    if code == "001": send_text(conn,"002")
-    username = recv_text(conn)
-    (ip,port) = ip_and_port(conn)
-
-    code = check_username(username)
-    send_text(conn,code)
-
     add_to_list(username,(ip,port))
 
-
+    while True:
+        print("---")
+        response = ""
+        send_text(conn,persons_ready(username))
+        response = recv_text(conn,id)
+        if response == "005":
+            time.sleep(5)
+        else:
+            print(response)
+            
     
 
 
-    
 
 
-    socket.close()
+def main(): # fonction principale
+    id_count = 0
+
+    reset_list()
+    initialisation(5566)
+    while True:
+        wait_connection(id_count)
+        id_count += 1
+
 
 main()
